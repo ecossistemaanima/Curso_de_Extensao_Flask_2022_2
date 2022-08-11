0d931114e0cff2ec3f206d7f75c0fc2893d0a9b3
Henrique Poyatos <henrique.poyatos@saojudas.br>
2022-08-10 22:08:42 -0300

    Passando vari';avel produtos estilo dicionário.

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from flask import Flask, render_template
 app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
-    return render_template("alo.html"), 200
+    #criar uma variável com o meu nome
+    nome="Henrique Poyatos"
+    produtos = [
+    {"nome": "Ração", "preco": 199.99},
+    {"nome": "Playstation", "preco": 1999.99}]
+
+    return render_template("alo.html", n=nome, aProdutos=produtos), 200
 
 app.run()
 
