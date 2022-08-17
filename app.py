7897cea12d2276f2192687ee580c97bc015e3a4b
Henrique Poyatos <henrique.poyatos@saojudas.br>
2022-08-17 19:55:11 -0300

    Criação de uma nova rota /teste

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from flask import Flask, render_template
 app = Flask("projeto")
+
+# Rota Raiz
 @app.route("/")
 def ola_mundo():
     #criar uma variável com o meu nome
@@ -10,6 +12,12 @@ def ola_mundo():
     {"nome": "Playstation", "preco": 1999.99}]
 
     return render_template("alo.html", n=nome, aProdutos=produtos), 200
+
+# Nova Rota Teste
+@app.route("/teste")
+@app.route("/teste/<variavel>")
+def funcao_teste(variavel = ""):
+    return "Nova rota teste<br>Variável: {}".format(variavel), 200
 
 app.run()
 
