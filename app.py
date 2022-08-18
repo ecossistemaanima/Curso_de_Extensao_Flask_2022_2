2ff47ef07f7fdb8f90855f457f532b929d28283b
Henrique Poyatos <henrique.poyatos@saojudas.br>
2022-08-17 21:41:55 -0300

    Exemplo com Sessão de usuário, simulando ambiente de login/senha.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # coding: utf-8
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask("projeto")
+# Criando uma chave de criptografia
+app.secret_key = "ASFAFQER#r#@r@#$r%$t@$gtgdfadfadfeaf"
 
 # Rota Raiz
 @app.route("/")
@@ -33,8 +35,29 @@ def form_recebe():
         return "Nome: {}".format(nome), 200
     else:
         return "Não pode chamar direto no GET", 200
-     
 
+# Rota form de login
+@app.route("/login")
+def login():
+    return render_template("login.html"), 200
+
+# Rota para validar o formulário
+@app.route("/login_validar", methods=["POST"])
+def login_validar():
+    if request.form["usuario"] == "poyatos" and request.form["senha"] == "12345":
+        session["usuario"] = request.form["usuario"]
+        session["codigo"] = 1
+        return redirect(url_for("acesso_restrito"))
+    else:
+        return "Usuário/senha inválidos, digite novamente.", 200
+
+# Rota para a área restrita
+@app.route("/restrito")
+def acesso_restrito():
+    if session["codigo"] == 1:
+        return "Bem-vindo à area restrita!!<br>Usuário: {}<br>Código: {}".format(session["usuario"], session["codigo"]), 200
+    else:
+        return "Acesso inválido", 200
 
 app.run()
 
